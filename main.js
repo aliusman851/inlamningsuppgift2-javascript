70e8e6e342e8b4567ca15cb92aab8c808cf8a7a5
alius <alius@LAPTOP-NNR66SG1>
2021-02-11 11:15:49 +0100

    try and catch statemrnt.

--- main.js
+++ main.js
@@ -13,34 +13,43 @@ let temperture = document.querySelector(".temperture");
 let condition = document.querySelector(".condition");
 const cb = document.querySelector('#weather');
         
-button.addEventListener('click', () => {
+button.addEventListener('click', async () => {
+    try{
 
-
-     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value +
-     '&appid=a89ac303d22fef22631095641fa4dcf1')
-        
+    const serverresponse = await  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value +
+     '&appid=a89ac303d22fef22631095641fa4dcf1');
+     if(serverresponse.ok) {
+     const information = await serverresponse.json();
+       /* 
         .then(Response => Response.json())
         .then(data => {
-            currentweather.innerHTML ='Current Weather';
+            currentweather.innerHTML ='Current Weather'; */
             border.setAttribute("style", "border: solid;");
-            let nameofcity = data["name"];
-            
-            let currenttem = data["main"]["temp"];
+            let nameofcity = information.name;
+           
+            let currenttem = information["main"]["temp"];
             let keltocel = currenttem-273.15;
-            let currentcond = data["weather"][0]["main"];
+            let currentcond = information["weather"][0]["main"];
             
             cityname.innerHTML = nameofcity;
+            
              writetemperture.innerHTML = 'Temperture';
             temperture.innerHTML = keltocel;
              writecondition.innerHTML ='Condition';
             condition.innerHTML = currentcond;
-            
+        } else {
+            alert("wrong city name")
+        }
+    }catch(error) {
+        alert("wrong city")
+    }
                 
             
-            
 
-        })
+
+        });
         
+    
        
 
 
@@ -48,7 +57,6 @@ button.addEventListener('click', () => {
 
 
    
-});
 button.addEventListener('click', () => {
     
     topattraction.innerHTML ='Top Attraction';
