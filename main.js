f6f30c02c2b5c6c31abe1f652e166b311a0e6d99
alius <alius@LAPTOP-NNR66SG1>
2021-02-13 00:58:36 +0100

    created function to get venu

--- main.js
+++ main.js
@@ -8,59 +8,62 @@ let attractionborder = document.querySelector(".cards");
 let cityname = document.querySelector(".name");
 let writetemperture = document.querySelector(".tem");
 let writecondition = document.querySelector(".con");
-
 let temperture = document.querySelector(".temperture");
 let condition = document.querySelector(".condition");
+let foodpoint1 = document.querySelector("attractionname");
 const cb = document.querySelector('#weather');
-        
-button.addEventListener('click', async () => {
-    try{
 
-    const serverresponse = await  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value +
-     '&appid=a89ac303d22fef22631095641fa4dcf1');
-     if(serverresponse.ok) {
-     const information = await serverresponse.json();
-       /* 
-        .then(Response => Response.json())
-        .then(data => {
-            currentweather.innerHTML ='Current Weather'; */
+button.addEventListener('click', async () => {
+    try {
+
+        const serverresponse = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value +
+        '&appid=a89ac303d22fef22631095641fa4dcf1');
+        if (serverresponse.ok) {
+            const information = await serverresponse.json();
+            /* 
+             .then(Response => Response.json())
+             .then(data => {
+                 currentweather.innerHTML ='Current Weather'; */
             border.setAttribute("style", "border: solid;");
             let nameofcity = information.name;
-           
+
             let currenttem = information["main"]["temp"];
-            let keltocel = currenttem-273.15;
+            let keltocel = currenttem - 273.15;
             let currentcond = information["weather"][0]["main"];
-            
+
             cityname.innerHTML = nameofcity;
-            
-             writetemperture.innerHTML = 'Temperture';
+            writetemperture.innerHTML = 'Temperture';
             temperture.innerHTML = keltocel;
-             writecondition.innerHTML ='Condition';
+            writecondition.innerHTML = 'Condition';
             condition.innerHTML = currentcond;
         } else {
-            alert("wrong city name")
+            alert("something is missing ")
         }
-    }catch(error) {
-        alert("wrong city")
+    } catch (error) {
+        alert("wrong city name ")
     }
-                
-            
+});
+
+const CLIENT_ID = "H3TXBYEDVD2ETUGDV2E0HY5GFOXMO1JXY13CX51TP12B2ZA2";
+const CLIENT_SECRET = "WFI5YNVOYWXAISJUWLD40O1VOZJZNVHLS5IYIPTK4VUPSQOK";
 
 
-        });
-        
-    
-       
 
-
-        
-
-
-   
 button.addEventListener('click', () => {
     
-    topattraction.innerHTML ='Top Attraction';
-    attractionborder.setAttribute("style", "border: solid;");
-    
-    
+    const CLIENT_ID = "H3TXBYEDVD2ETUGDV2E0HY5GFOXMO1JXY13CX51TP12B2ZA2";
+const CLIENT_SECRET = "WFI5YNVOYWXAISJUWLD40O1VOZJZNVHLS5IYIPTK4VUPSQOK";
+    fetch('https://api.foursquare.com/v2/venues/explore?cat=food&near='+inputvalue.value+'&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&v=20210212&limit=10')
+   
+    .then(response => response.json())
+    .then(result =>console.log(result))
+    topattraction.innerHTML = 'Top Attraction'
+    attractionborder.setAttribute("style", "border: solid;")
+    /*
+let resturantname = response.name;
+foodpoint1.innerHTML=resturantname;
+    */
+   
+   
+
 });
