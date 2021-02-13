769e3bb013602ea58a5c4d82218f793f49bb7001
alius <alius@LAPTOP-NNR66SG1>
2021-02-13 12:21:48 +0100

    fetch the name of first topattraction.

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ button.addEventListener('click', async () => {
         '&appid=a89ac303d22fef22631095641fa4dcf1');
         if (serverresponse.ok) {
             const information = await serverresponse.json();
+            
             /* 
              .then(Response => Response.json())
              .then(data => {
@@ -37,23 +38,30 @@ button.addEventListener('click', async () => {
             writecondition.innerHTML = 'Condition';
             condition.innerHTML = currentcond;
         } else {
-            alert("something is missing ")
+            alert("wrong city name ")
         }
     } catch (error) {
-        alert("wrong city name ")
+        alert("given city name is incorrect")
     }
 });
 
-button.addEventListener('click', () => {
+button.addEventListener('click', async() => {
     
     const CLIENT_ID = "H3TXBYEDVD2ETUGDV2E0HY5GFOXMO1JXY13CX51TP12B2ZA2";
-    const CLIENT_SECRET = "WFI5YNVOYWXAISJUWLD40O1VOZJZNVHLS5IYIPTK4VUPSQOK";
-    fetch('https://api.foursquare.com/v2/venues/explore?cat=food&near='+inputvalue.value+'&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&v=20210212&limit=10')
-   
-    .then(response => response.json())
-    .then(result =>console.log(result))
-    topattraction.innerHTML = 'Top Attraction'
-    attractionborder.setAttribute("style", "border: solid;")
+    const CLIENT_SECRET = "JENIKXJM3KFBX2NMGFBRQKCOVSSKLURXCKV45JHTEL4XCP3J";
+    const serverresponse = await fetch('https://api.foursquare.com/v2/venues/explore?cat=topPicks&near='+inputvalue.value+'&client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&v=20210213&limit=10')
+   const data = await serverresponse.json();
+   let place = data.response.groups[0].items[0].venue.name;
+    //.then(response => response.json())
+   // .then(result => console.log(result));
+   topattraction.innerHTML = 'Top Attraction'
+   attractionborder.setAttribute("style", "border: solid;")
+
+let placename = document.querySelector(".attractionname1");
+placename.innerHTML = place;
+
+    });
+    
     /*
 let resturantname = response.name;
 foodpoint1.innerHTML=resturantname;
@@ -61,4 +69,3 @@ foodpoint1.innerHTML=resturantname;
    
    
 
-});
